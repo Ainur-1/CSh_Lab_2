39d25fd1646ba7cd8d650f6bae22c4e14233c054
Ainur <76821737+Ainur-1@users.noreply.github.com>
2022-01-07 00:08:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace CSh_Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex_1();
+            Ex_2();
+        }
+
+        static void Ex_1()
+        {
+            int x_begin = -10, x_end = 8;
+            double x, y, dx;
+
+            Console.Write("dx = ");
+            dx = Convert.ToDouble(Console.ReadLine());
+
+            x = x_begin;
+            y = -3;
+
+            Console.WriteLine("     x     |    y     ");
+
+            while (x <= x_end)
+            {
+                if (x <= -8) y = -3;
+                if (x > -8 & x <= -3) y = 1.6 * x + 1.8;
+                if (x > -3 & x <= 3) y = Math.Sqrt(9 - x * x);
+                if (x > 3 & x <= 5) y = x - 3;
+                if (x > 5) y = 3;
+
+                Console.WriteLine($"{Math.Round(x, 3),10} | {Math.Round(y, 3),10}");
+                x += dx;
+            }
+
+        }
+
+        static void Ex_2()
+        {
+            int a;
+            for (int i = 10; i < 100; i++)
+            {
+                a = i * i;
+                if (a / 1000 == 0)
+                {
+                    if (a / 100 == a % 10) Console.WriteLine(a);
+                }
+                else
+                {
+                    if ((a / 1000 == a % 10) & ((a / 100) % 10 == (a % 100) / 10)) Console.WriteLine(a);
+                }
+                
+            }
+        }
+    }
+}
